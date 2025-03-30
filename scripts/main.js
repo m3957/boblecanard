448d402bd139094445ab06bf7484d5457a923b65
m3957 <86982093+m3957@users.noreply.github.com>
2025-03-30 10:46:23 -0400

    Ajouté un Bob tournant

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,17 @@
-const myImage = document.querySelector("img");
+// Get the third image element
+const thirdImage = document.querySelectorAll("img")[2];
 const myButton = document.querySelector("button");
 
-/*
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/duck.webp") {
-    myImage.setAttribute("src", "images/euh.png");
-  } else {
-    myImage.setAttribute("src", "images/duck.webp");
-  }
-};
-*/
-
 const images = [
-    "images/duck.webp",
-    "images/euh.png",
-    "images/mamie.jpg",
-    "images/timmy.jpg"
-  ];
-  
-  let currentIndex = 0;
-  
-  myButton.onclick = () => {
-    currentIndex = (currentIndex + 1) % images.length; // Increment index and wrap around
-    myImage.setAttribute("src", images[currentIndex]);
-  };
+	"images/duck.webp",
+	"images/euh.png",
+	"images/mamie.jpg",
+	"images/timmy.jpg",
+];
+
+let currentIndex = 0;
+
+myButton.onclick = () => {
+	currentIndex = (currentIndex + 1) % images.length; // Increment index and wrap around
+	thirdImage.setAttribute("src", images[currentIndex]);
+};
